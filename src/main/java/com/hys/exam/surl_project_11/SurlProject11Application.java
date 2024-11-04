c842fd62e2a6a0d44b2fad25ea314c67b64b4beb
qBlackbirdp <blackbird9711@gmail.com>
2024-11-04 15:13:22 +0900

    프로젝트 세팅

---
+++ src/main/java/com/hys/exam/surl_project_11/SurlProject11Application.java
@@ -0,0 +1,13 @@
+package com.hys.exam.surl_project_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurlProject11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurlProject11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hys/exam/surl_project_11/SurlProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hys.exam.surl_project_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlProject11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
